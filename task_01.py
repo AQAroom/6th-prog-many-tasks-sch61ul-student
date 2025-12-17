87cdd1600decde7669446d1cfbf64f62cbe91ead
sch61ul-student <sch61ul@gmail.com>
2025-12-17 10:03:20 +0400

    Update task_01.py

--- task_01.py
+++ task_01.py
@@ -1,1 +1,18 @@
-# Здесь пишем свой код
+a = int( input() )
+z = input()
+b = int(input())
+if z == "+":
+    print(a +  b)
+elif z == "-":
+    print(a - b)
+elif z ==  "*":
+    print(a * b)
+elif z == "/":
+    if b == 0:
+        print("error")
+    else:
+        print(a / b)
+elif z == "^":
+    print(a  ** b)
+else:
+    print( "Неизвестная операция" )
